2f936b9f8c97f06bd4c8797523b8d73d5a039636
Shani Herskowitz <shanizoe@gmail.com>
2018-05-29 21:38:46 +0300

    added speed

--- Shapes/SettingsScene.swift
+++ Shapes/SettingsScene.swift
@@ -13,15 +13,22 @@ class SettingsScene: SKScene {
   var sound: SKSpriteNode?
   var credits: SKLabelNode?
   var backButton: SKSpriteNode?
+  var rabbit: SKSpriteNode?
+  var snail: SKSpriteNode?
+  var cheetta: SKSpriteNode?
+  var circle: SKSpriteNode?
   var soundOn =  true
   
-  //let playButtonTex = SKTexture(imageNamed: "play")
   
   override func didMove(to view: SKView) {
     
     sound = self.childNode(withName: "sound") as? SKSpriteNode
     credits = self.childNode(withName: "credits") as? SKLabelNode
     backButton = self.childNode(withName: "back") as? SKSpriteNode
+    circle = self.childNode(withName: "circle") as? SKSpriteNode
+    snail = self.childNode(withName: "snail") as? SKSpriteNode
+    rabbit = self.childNode(withName: "rabbit") as? SKSpriteNode
+    cheetta = self.childNode(withName: "cheetta") as? SKSpriteNode
     
   }
   
@@ -30,7 +37,8 @@ class SettingsScene: SKScene {
       let pos = touch.location(in: self)
       let node = self.atPoint(pos)
       
-      if node == sound {
+      switch node {
+      case sound:
         if (soundOn) {
           sound?.texture = SKTexture(imageNamed: "sound off");
           sound?.size = CGSize(width: 231, height: 231);
@@ -41,7 +49,8 @@ class SettingsScene: SKScene {
           sound?.position = CGPoint(x: (sound?.position.x)! + 12, y: (sound?.position.y)! + 2)
         }
         soundOn = !soundOn
-      } else if node == credits {
+        
+      case credits:
         if view != nil {
           let transition:SKTransition = SKTransition.doorway(withDuration: 1)
           let scene:SKScene = CreditsScene(fileNamed: "CreditsScene")!
@@ -49,7 +58,8 @@ class SettingsScene: SKScene {
           scene.scaleMode = .aspectFill
           self.view?.presentScene(scene, transition: transition)
         }
-      } else if node == backButton {
+        
+      case backButton:
         if view != nil {
           let transition:SKTransition = SKTransition.fade(withDuration: 1)
           let scene:SKScene = MenuScene(fileNamed: "MenuScene")!
@@ -57,7 +67,16 @@ class SettingsScene: SKScene {
           
           self.view?.presentScene(scene, transition: transition)
         }
+        
+      case snail: circle?.position = CGPoint(x: -354.211, y: -626.22)
+        
+      case rabbit: circle?.position = CGPoint(x: -2.276, y: -626.22)
+        
+      case cheetta: circle?.position = CGPoint(x: 336.743, y: -626.22)
+        
+      default: print("no button")
+      
+    }
     }
   }
-}
 }
